7e5868a7086861f354fa03a4f353fd8c652fc409
Hetang Shah <hetang.shah@gmail.com>
2016-07-28 12:29:18 -0700

    Organizing dependencies.

--- infra/src/main/java/com/infra/managers/DataManager.java
+++ infra/src/main/java/com/infra/managers/DataManager.java
@@ -1,24 +1,34 @@
 package com.infra.managers;
 
-import android.content.Context;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.infra.managers.deserializer.ErrorDeserializer;
 import com.infra.managers.requests.MultipartWithJsonResponseConverterFactory;
 import com.infra.managers.requests.Service;
+import com.infra.managers.responses.ServiceResponse;
 import com.infra.managers.utils.MockServiceUtil;
 import com.infra.managers.utils.ServiceUtil;
+import com.infra.logging.util.LogUtils;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
 
 public abstract class DataManager<T> {
+    public static String TAG = DataManager.class.getName();
+
+    /**
+     * Needs to override based on Product requirement
+     */
+    protected static final int API_INTERVAL = 30;
+    protected static final int API_RETRY = 3;
+    protected static final int API_RETRY_START_MILLISECOND = 1000;
+
     private DataObserver<T> observer;
-    private Context context;
-    protected Gson mapper = new GsonBuilder().create(); //Creating common Gson mapper to be used in child managers
 
     private static RetrofitServicePair serviceHolder;
     private static RetrofitServicePair multipartServiceHolder;
@@ -34,10 +44,6 @@ public abstract class DataManager<T> {
 
     static {
         init();
-    }
-
-    public DataManager(Context context) {
-        this.context = context;
     }
 
     private static void init() {
@@ -91,15 +97,45 @@ public abstract class DataManager<T> {
         DataManager.multipartServiceHolder.setService(multipartService);
     }
 
-    protected DataObserver<T> getObserver() {
+    public DataObserver<T> getObserver() {
         return observer;
     }
 
-    protected void setObserver(DataObserver<T> observer) {
+    public void setObserver(DataObserver<T> observer) {
         this.observer = observer;
     }
 
-    protected Context getContext() {
-        return context;
+    /**
+     * Unpack a api response into a wrapper, capturing both the response's
+     * data and connection state.
+     *
+     * @param call the Retrofit call object
+     * @return the fully constructed response wrapper
+     */
+    protected ServiceResponse<T> unpackResult(Call<T> call) {
+        ServiceResponse<T> result = new ServiceResponse<T>();
+
+        try {
+            Response<T> response = call.execute();
+
+            if(response.isSuccessful()) {
+                result.setResponseState(ServiceResponse.ResponseState.OK);
+                result.setData(response.body());
+                result.setHttpResponseCode(response.code());
+            } else {
+                result.setResponseState(ServiceResponse.ResponseState.parseHttpErrorCode(response.code()));
+                result.setHttpResponseCode(response.code());
+                result.setResponseMessage(response.message());
+            }
+        } catch (Exception e) {
+            // Specifically ignoring Throwable because we can't handle JVM errors anyway
+            result.setResponseState(ServiceResponse.ResponseState.OTHER_ERROR);
+            result.setResponseMessage(e.getMessage());
+            LogUtils.e(TAG, "Error while processing the response: " + e.getMessage(), e);
+        }
+
+        return result;
     }
+
+    protected abstract Observable<ServiceResponse<T>> getRxObservable();
 }
